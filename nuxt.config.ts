41998eb9852e5d4a5421bc346706b9ca179f6647
ibingbo <bingbo.zh@gmail.com>
2026-02-05 16:19:13 +0800

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+  ui: {
+    fonts: false
+  },
+  routeRules: {
+    '/': {prerender: true}
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  },
+  nitro: {
+    output: {
+      publicDir: 'docs' // GitHub Pages 可直接读取
+    }
+  },
+  ssr: false
+})
